6f7969388f2ecc2a38c5a77baf53c1d8535060fe
THIAGO S. GONÇALVES <thiago_gub@hotmail.com>
2021-07-12 20:43:00 -0300

    First commint

---
+++ TestePrimeiro/index.js
@@ -0,0 +1,64 @@
+const {app, BrowserWindow, ipcMain} = require('electron');
+const ipc = ipcMain
+
+// JANELA PRINCIPAL
+var mainWindow = null
+async function createWindow() {
+    mainWindow = new BrowserWindow({
+        width:400,
+        height:600,
+        minWidth:400,
+        minHeight:600,
+        frame: false,
+        resizable: true,
+        show: false,
+        backgroundColor: '#1E222D',
+        webPreferences:{
+            nodeIntegration: true,
+            contextIsolation: false,
+            devTools:true
+        }
+    })
+
+    mainWindow.loadFile('./src/index.html')
+
+    mainWindow.on('ready-to-show', () => {
+        mainWindow.show()
+    })
+
+    // MINIMIZE WINDOW
+    ipc.on('minimizeApp', () => {
+        console.log('MINIMIZADO')
+        mainWindow.minimize()
+    })
+
+    // CLOSE WINDOW
+    ipc.on('closeApp', () => {
+        console.log('FECHADO')
+        mainWindow.close()
+    })
+
+    // MAXIMIZE WINDOW
+    ipc.on('maximizeRestoreApp', () => {
+
+        if (mainWindow.isMaximized()) {
+            console.log('RESTORE')
+            mainWindow.restore()
+        } else {
+            console.log('MAXIMIZADO')
+            mainWindow.maximize()   
+        }
+    })
+
+
+}
+
+// ON READY
+app.whenReady().then(createWindow)
+
+// ACTIVATE
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
---
+++ TestePrimeiro/src/JS/LoginMain.js
@@ -0,0 +1,44 @@
+const { ipcRenderer } = require('electron')
+const ipc = ipcRenderer
+
+
+// MINIMIZE WINDOW
+minimizeBtn.addEventListener('click', () => {
+    ipc.send('minimizeApp')
+})
+
+// CLOSE WINDOW
+closeBtn.addEventListener('click', () => {
+    ipc.send('closeApp')
+})
+
+// MAXIMIZE WINDOW
+maxmizeBtn.addEventListener('click', () => {
+    ipc.send('maximizeRestoreApp')
+})
+
+// SHORTCUT com KEYBOARDEVENT ////////////////////////////////////////////
+
+window.addEventListener('keyup', ShortcutLeftBar, false)
+
+function ShortcutLeftBar(key) {
+    const EnterAcesso = document.querySelector('#buttonEnter')
+
+    if (key.keyCode == "13") {
+        EnterAcesso.click()
+    }
+}
+
+/////////////////////////////////////////////////////////////////////
+
+function acessoLog (form){
+    form.nome.value = form.nome.value.toUpperCase()
+
+    if ((form.nome.value == "THIAGO GONÇALVES" && form.senha.value == "91318201") || (form.nome.value == "KAUE" && form.senha.value == "123")) {
+        location = "PageMain.html"
+    } else {
+        form.nome.value=""
+        form.senha.value=""
+        alert ("Não cadastrado")
+    }
+}

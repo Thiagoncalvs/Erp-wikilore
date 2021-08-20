358059b672acbc862796f82343de45d10bef15df
THIAGO S. GONÇALVES <thiago_gub@hotmail.com>
2021-08-20 16:31:44 -0300

    ssss

--- server-side/configserver.js
+++ server-side/configserver.js
@@ -2,18 +2,19 @@ const serverhttp = require('http')
 const filefs = require('fs')
 const thepath = require('path')
 
-serverhttp.createServer((req, res) =>{
 
+serverhttp.createServer((req, res) =>{
+    
    const thefile = req.url === '/' ? 'index.html' : req.url
    const twofile = req.url === '/home' ? 'PageMain.html' : req.url
 
    if (req.url === '/') {
     const filePath = thepath.join(__dirname, 'src', thefile)  
-    
+    // 
     filefs.readFile(
         filePath,
         (err, content) =>{res.end(content)}
-    )  
+    )
    } else{
     const filePath = thepath.join(__dirname, 'src', twofile)
 
